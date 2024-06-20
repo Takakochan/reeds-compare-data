2c8fb403cd9dbd236e3bed4c273e3be5eb3e9383
TKK <73033322+Takakochan@users.noreply.github.com>
2024-06-20 12:17:21 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+from ftplib import FTP_TLS
+import ftplib
+from io import BytesIO
+import io
+
+
+ftp = FTP_TLS('minty-web.com')
+print('going to the sever....')
+print('loged in....')
+ftp.login('kzqlqkbl', '4*2arARaY2.uR2')
+ftp.prot_p()
+ftp.set_pasv('true')
+ftp.cwd('public_html/kzqlqkbl/datas')
+flo = BytesIO()
+flo.seek(0)
+data = pd.read_csv(flo, encoding='UTF8',sep=";")
+data = data.set_index(['Length'])
+df = pd.DataFrame(data)
+
+columns = st.sidebar.multiselect("Columns:", df.columns)
+filter = st.sidebar.radio("Choose by:", ("inclusion", "exclusion"))
+
+if filter == "exclusion":
+    columns = [col for col in df.columns if col not in columns]
+df[columns]
+data1 = df[columns]
+#data = data.drop['Unnamed']
+df = pd.DataFrame(data)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+if st.sidebar.button('Plot graph'):
+    data1.plot()
+    plt.show()
+    st.pyplot()
